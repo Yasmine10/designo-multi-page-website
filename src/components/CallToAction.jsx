2981cfe78af31d221afa216303134b0a5bef73ad
Yasmine De Wolf <yasmine@fw4.be>
2023-03-13 16:40:20 +0100

    added call to action html

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 function CallToAction() {
     return (
         <section className="call-to-action">
-            Call to action
+            <div className="container">
+                <div className="call-to-action__content">
+                    <h2>Let's talk about your project</h2>
+                    <p>Ready to take it to the next level? Contact us today and find out how our expertise can help your business grow.</p>
+                </div>
+                <Link to="/contact" className="btn btn--primary">Get in touch</Link>
+            </div>
         </section>
     );
 }
