816998937f7b8d12ae2aa67fa48834d5659e67db
Yasmine De Wolf <yasmine@fw4.be>
2023-03-14 10:43:23 +0100

    added call to action styling + made it responsive

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 function CallToAction() {
     return (
         <section className="call-to-action">
-            <div className="container">
+            <div className="container bg-primary-400 txt-primary-100">
                 <div className="call-to-action__content">
-                    <h2>Let's talk about your project</h2>
+                    <h2 className="h2">Let's talk about your project</h2>
                     <p>Ready to take it to the next level? Contact us today and find out how our expertise can help your business grow.</p>
                 </div>
-                <Link to="/contact" className="btn btn--primary">Get in touch</Link>
+                <Link to="/contact" className="call-to-action__btn | btn btn--primary">Get in touch</Link>
             </div>
         </section>
     );
