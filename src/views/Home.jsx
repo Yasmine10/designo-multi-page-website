7eb13d206e27f71f0e2a7347f8960e60b83ffd6d
Yasmine De Wolf <yasminedewolf@hotmail.com>
2023-03-19 16:59:09 +0100

    added styling home hero section + made responsive

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -5,11 +5,11 @@ import HeroImg from "../assets/home/desktop/image-hero-phone.png";
 function Home (props) {
     return (
         <main id="main" className="main">
-            <section className="home-hero">
+            <section className="home-hero | container--full-mobile spacing-l">
                 <div className="home-hero__content">
-                    <h1 className="h1">Award-winning custom designs and digital branding solutions</h1>
-                    <p>With over 10 years in the industry, we are experienced in creating fully responsive websites, app design, and engaging brand experiences. Find out more about our services.</p>
-                    <Link to="/our-company" className="btn btn--primary">Learn more</Link>
+                    <h1 className="home-hero__title | h1">Award-winning custom designs and digital branding solutions</h1>
+                    <p className="home-hero__text">With over 10 years in the industry, we are experienced in creating fully responsive websites, app design, and engaging brand experiences. Find out more about our services.</p>
+                    <Link to="/our-company" className="home-hero__btn | btn btn--primary">Learn more</Link>
                 </div>
                 <div className="home-hero__img-container">
                     <img src={HeroImg} alt=""/>
