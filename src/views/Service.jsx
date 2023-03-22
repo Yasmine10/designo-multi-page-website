d2c29156acb1e6caa595647ad0b033428c86d860
Yasmine De Wolf <yasminedewolf@hotmail.com>
2023-03-22 11:17:59 +0100

    added html service hero section

--- src/views/Service.jsx
+++ src/views/Service.jsx
@@ -2,7 +2,6 @@ import { useParams } from "react-router-dom";
 
 import servicesData from "../data/services.json";
 
-import Intro from "../components/service/Intro.jsx";
 import Project from "../components/service/Project.jsx";
 import OtherServices from "../components/service/OtherServices.jsx";
 
@@ -15,7 +14,10 @@ function Service() {
 
     return (
       <main id="main" className="main">
-          <Intro service={data} />
+          <section className={`service-intro ${data.slug} | container--full-mobile`}>
+              <h1 className="service-intro__title | h1">{data.name}</h1>
+              <p className="service-intro__text">{data.intro_text}</p>
+          </section>
 
           <section className="service-projects | container">
             <ul className="service-projects__list">
