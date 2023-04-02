88800dd8c60e9b896d557ce95945369468c36527
Yasmine De Wolf <yasminedewolf@hotmail.com>
2023-04-02 14:36:05 +0200

    reset form input values after submit

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -4,58 +4,70 @@ import Locations from "../components/Locations.jsx";
 
 function Contact(props) {
 
-    const [ formData, setFormData ] = useState({});
+    const [ formData, setFormData ] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        message: ""
+    });
     const [ formErrors, setFormErrors ] = useState({});
-    const [ valid, setValid ] = useState();
 
     const isEmail = (email) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
-
-    const validation = (formInputs) => {
-        const emptyErrorMessage = "Can't be empty!";
-        const emailFormatErrorMessage = "Must be a valid email address!";
-
-        const errors = {};
-        let valid = true;
-
-        if (!formInputs.name.value) {
-            errors.name = emptyErrorMessage;
-            valid = false;
-        }
-
-        if (!formInputs.email.value) {
-            errors.email = emptyErrorMessage;
-            valid = false;
-        } else if (!isEmail(formInputs.email.value)) {
-            errors.email = emailFormatErrorMessage;
-            valid = false;
-        }
-
-        if (!formInputs.phone.value) {
-            errors.phone = emptyErrorMessage;
-            valid = false;
-        }
-
-        if (!formInputs.message.value) {
-            errors.message = emptyErrorMessage;
-            valid = false;
-        }
-
-        setValid(valid);
-        setFormErrors(errors);
-    }
 
     const handleChange = (e) => {
         const { name, value } = e.target;
         setFormData({...formData, [name]: value});
     }
 
+    const formValidation = () => {
+        const emptyErrorMessage = "Can't be empty!";
+        const emailFormatErrorMessage = "Must be a valid email address!";
+
+        const errors = {};
+        let valid = true;
+
+        if (!formData.name) {
+            errors.name = emptyErrorMessage;
+            valid = false;
+        }
+
+        if (!formData.email) {
+            errors.email = emptyErrorMessage;
+            valid = false;
+        } else if (!isEmail(formData.email)) {
+            errors.email = emailFormatErrorMessage;
+            valid = false;
+        }
+
+        if (!formData.phone) {
+            errors.phone = emptyErrorMessage;
+            valid = false;
+        }
+
+        if (!formData.message) {
+            errors.message = emptyErrorMessage;
+            valid = false;
+        }
+
+        setFormErrors(errors);
+
+        return valid;
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        validation(e.target);
+        let valid = formValidation();
 
         if (valid) {
             alert("Thanks for filling in the form!");
+
+            setFormData({
+                name: "",
+                email: "",
+                phone: "",
+                message: ""
+            });
         }
     }
 
@@ -72,22 +84,22 @@ function Contact(props) {
                     <form onSubmit={handleSubmit}>
                         <div className="form-group">
                             <label htmlFor="contact-form__name" className="sr-only">Name</label>
-                            <input type="text" id="contact-form__name" name="name" placeholder="Name" onChange={handleChange} />
+                            <input type="text" id="contact-form__name" name="name" placeholder="Name" value={formData.name} onChange={handleChange} />
                             <small className={"input-error" + (formErrors.name ? " invalid" : "")}>{formErrors.name}</small>
                         </div>
                         <div className="form-group">
                             <label htmlFor="contact-form__email" className="sr-only">Email Address</label>
-                            <input type="email" id="contact-form__email" name="email" placeholder="Email Address" onChange={handleChange} />
+                            <input type="email" id="contact-form__email" name="email" placeholder="Email Address" value={formData.email} onChange={handleChange} />
                             <small className={"input-error" + (formErrors.email ? " invalid" : "")}>{formErrors.email}</small>
                         </div>
                         <div className="form-group">
                             <label htmlFor="contact-form__phone" className="sr-only">Phone</label>
-                            <input type="text" id="contact-form__phone" name="phone" placeholder="Phone" onChange={handleChange} />
+                            <input type="text" id="contact-form__phone" name="phone" placeholder="Phone" value={formData.phone} onChange={handleChange} />
                             <small className={"input-error" + (formErrors.phone ? " invalid" : "")}>{formErrors.phone}</small>
                         </div>
                         <div className="form-group">
                             <label htmlFor="contact-form__message" className="sr-only">Your message</label>
-                            <textarea id="contact-form__message" name="message" rows="4" placeholder="Your message" onChange={handleChange} />
+                            <textarea id="contact-form__message" name="message" placeholder="Your message" value={formData.message} onChange={handleChange} />
                             <small className={"input-error" + (formErrors.message ? " invalid" : "")}>{formErrors.message}</small>
                         </div>
                         <button type="submit" className="contact-form__btn | btn btn--primary" formNoValidate={true}>Submit</button>
