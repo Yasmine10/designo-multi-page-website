eeaa583e88c79b876216c25ebf40bf4d1cc0c9b2
Yasmine De Wolf <yasminedewolf@hotmail.com>
2023-05-02 18:42:13 +0200

    added about page content sections (html + styling)

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -2,6 +2,12 @@ import ImgHeroMobile from "../assets/about/mobile/image-about-hero.jpg";
 import ImgHeroTablet from "../assets/about/tablet/image-about-hero.jpg";
 import ImgHeroDesktop from "../assets/about/desktop/image-about-hero.jpg";
 
+import ImgContent1Mobile from "../assets/about/mobile/image-world-class-talent.jpg";
+import ImgContent1Desktop from "../assets/about/desktop/image-world-class-talent.jpg";
+
+import ImgContent2Mobile from "../assets/about/mobile/image-real-deal.jpg";
+import ImgContent2Desktop from "../assets/about/desktop/image-real-deal.jpg";
+
 import Locations from "../components/Locations.jsx";
 
 function About(props) {
@@ -23,7 +29,39 @@ function About(props) {
                 </div>
             </section>
 
+            <section className="about-content about-content--left | container--full-mobile spacing-l">
+                <div className="about-content__img-container">
+                    <picture>
+                        <source srcSet={ImgContent1Desktop} media="(min-width: 1200px)" />
+                        <img src={ImgContent1Mobile} alt=""/>
+                    </picture>
+                </div>
+                <div className="about-content__content bg-secondary-200">
+                    <h2 className="title | h2 txt-primary-400">World-class talent</h2>
+                    <div className="text">
+                        <p>We are a crew of strategists, problem-solvers, and technologists. Every design is thoughtfully crafted from concept to launch, ensuring success in its given market. We are constantly updating our skills in a myriad of platforms.</p>
+                        <p>Our team is multi-disciplinary and we are not merely interested in form — content and meaning are just as important. We give great importance to craftsmanship, service, and prompt delivery. Clients have always been impressed with our high-quality outcomes that encapsulates their brand’s story and mission.</p>
+                    </div>
+                </div>
+            </section>
+
             <Locations />
+
+            <section className="about-content about-content--right | container--full-mobile spacing-l">
+                <div className="about-content__img-container">
+                    <picture>
+                        <source srcSet={ImgContent2Desktop} media="(min-width: 1200px)" />
+                        <img src={ImgContent2Mobile} alt=""/>
+                    </picture>
+                </div>
+                <div className="about-content__content | bg-secondary-200">
+                    <h2 className="title | h2 txt-primary-400">The real deal</h2>
+                    <div className="text">
+                        <p>As strategic partners in our clients’ businesses, we are ready to take on any challenge as our own. Solving real problems require empathy and collaboration, and we strive to bring a fresh perspective to every opportunity. We make design and technology more accessible and give you tools to measure success.</p>
+                        <p>We are visual storytellers in appealing and captivating ways. By combining business and marketing strategies, we inspire audiences to take action and drive real results.</p>
+                    </div>
+                </div>
+            </section>
         </main>
     );
 }
