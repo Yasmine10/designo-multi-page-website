fae51f72bf555f41b9a6a73b551800216a1b0131
Yasmine De Wolf <yasminedewolf@hotmail.com>
2023-05-06 15:50:08 +0200

    fix image web design service

--- src/components/OtherServices.jsx
+++ src/components/OtherServices.jsx
@@ -1,21 +1,20 @@
-import { Link } from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 
 function OtherServices(props) {
+
+    const location = useLocation();
+
     return (
-        <section className="service-other-services service | container">
+        <section className={`other-services ${location.pathname === '/' ? "home" : "service"} | container spacing-l`}>
             { props.services.map((service, index) => (
-                <Link to={`/services/${service.slug}`} className="service-other-services__service-link" key={index}>
+                <Link to={`/services/${service.slug}`} className="other-services__service-link" key={index}>
                     <div className="bg">
                         <picture>
                             { service.slug === 'web-design' ?
-                                <>
-                                    <source srcSet={`/assets/home/desktop/${service.images.small}`} media="(min-width: 1440px)" />
-                                    <source srcSet={`/assets/home/desktop/${service.images.small}`} media="(min-width: 1200px)" />
-                                </>
+                                <source srcSet={'/assets/home/desktop/' + (location.pathname === "/" ? service.images.large : service.images.small)} media="(min-width: 1200px)" />
                                 :
                                 <source srcSet={`/assets/home/desktop/${service.image}`} media="(min-width: 1200px)" />
                             }
-                            <source srcSet={`/assets/home/desktop/${service.image}`} media="(min-width: 1200px)" />
                             <source srcSet={`/assets/home/tablet/${service.image}`} media="(min-width: 768px)" />
                             <img src={`/assets/home/mobile/${service.image}`} alt="" />
                         </picture>
